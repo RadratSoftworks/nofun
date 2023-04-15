0c514205a054b4bc1c519b629024e5fe81192fb7
pent0 <l51492003231@163.com>
2023-04-16 02:42:36 +0700

    pip2/interpreter: Implement ADDQ

--- Assets/Scripts/PIP2/Interpreter/Instructions/Arithmetics.cs
+++ Assets/Scripts/PIP2/Interpreter/Instructions/Arithmetics.cs
@@ -1,9 +1,15 @@
 using Nofun.PIP2.Encoding;
+using Nofun.Util;
 
 namespace Nofun.PIP2.Interpreter
 {
     public partial class Interpreter : Processor
     {
+        private void ADDQ(TwoSourcesEncoding encoding)
+        {
+            Reg(encoding.d) = Reg(encoding.s) + BitUtil.SignExtend(encoding.t);
+        }
+
         private void SUB(TwoSourcesEncoding encoding)
         {
             Reg(encoding.d) = Reg(encoding.s) - Reg(encoding.t);
--- Assets/Scripts/PIP2/Interpreter/Interpreter.cs
+++ Assets/Scripts/PIP2/Interpreter/Interpreter.cs
@@ -18,7 +18,7 @@ namespace Nofun.PIP2.Interpreter
                 null, null, null, null, null, null, null, null,    // 0x00
                 null, I(SUB), null, null, null, null, null, null,    // 0x08
                 null, null, null, null, null, null, null, null,    // 0x10
-                null, null, null, null, null, null, null, null,    // 0x18
+                null, null, null, null, null, null, null, I(ADDQ),    // 0x18
                 null, null, null, null, null, null, null, null,    // 0x20
                 null, null, null, null, null, null, null, null,    // 0x28
                 null, null, null, null, null, null, null, null,    // 0x30
